3f79adbea647af5835067d34ef4644630f0286a8
LuisHenrick <luishenrick77@gmail.com>
2023-09-12 14:28:26 -0300

    ruby inicial

---
+++ desafio.rb
@@ -0,0 +1,14 @@
+#saida de dado
+print "Digite seu nome : "
+
+#Recebendo uma entrada do teclado 
+name  = gets.chomp
+
+print "Digite sua idade :"
+
+idade = gets.chomp
+
+#Saida utilizando puts
+#Use códigos ruby dentro de uma string com #(code)
+puts "Ola #{name} parabens por ter #{idade}"
+
